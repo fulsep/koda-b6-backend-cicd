18155f0dcbee88b7d27aa75b99f49e7c98c02437
fulsep <57238803+fulsep@users.noreply.github.com>
2026-03-17 11:06:35 +0700

    feat: add cors middleware and allow all origins

--- main.go
+++ main.go
@@ -14,8 +14,24 @@ type User struct {
 
 type Users []User
 
+func corsMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.Header("Access-Control-Allow-Origin", "*")
+		ctx.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
+		ctx.Header("Access-Control-Allow-Headers", "content-type,authorization")
+
+		if ctx.Request.Method == "OPTIONS" {
+			ctx.AbortWithStatus(http.StatusOK)
+		} else {
+			ctx.Next()
+		}
+	}
+}
+
 func main() {
 	r := gin.Default()
+
+	r.Use(corsMiddleware())
 
 	r.GET("/auth/login", func(ctx *gin.Context) {
 		var data User
